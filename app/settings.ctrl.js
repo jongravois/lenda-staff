939719beeb4dfad979d4ac0ab2170de3cb977b3d
Jon Gravois <jongravois@gmail.com>
2015-07-25 11:42:03 -0500

    Preferences I

--- app/settings.ctrl.js
+++ app/settings.ctrl.js
@@ -10,6 +10,22 @@
         function SettingsController() {
             /* jshint validthis: true */
             var vm = this;
+            vm.showViewOpts = false;
+            vm.showViewFltrs = false;
+
+            vm.togShowViewOpts = function() {
+                vm.showViewOpts = !vm.showViewOpts;
+            };
+            vm.togShowViewFltrs = function() {
+                vm.showViewFltrs = !vm.showViewFltrs;
+            };
+
+            vm.updateSettings = function() {
+                alert('Updating');
+            }
+
+            vm.user = JSON.parse(localStorage.getItem('user'));
+            console.log(vm.user);
 
             //////////
 
