ce37c9ffcca92aaac7ce7ad5ccbb3ef5ea115896
Jon Gravois <jongravois@gmail.com>
2015-07-25 12:03:57 -0500

    Library Structure

--- _modules/Library/library.routes.js
+++ _modules/Library/library.routes.js
@@ -10,9 +10,53 @@
                     templateUrl: '_modules/Library/_views/library.view.html',
                     controller: 'LibraryController as library'
                 })
-                .state('library.home', {
-                    url: '/home',
-                    templateUrl: '_modules/Library/_views/home.tmpl.html',
+                .state('library.classroom', {
+                    url: '/classroom',
+                    templateUrl: './_modules/Library/_views/classroom/classroom.html',
+                    controller: 'LibraryController as library'
+                })
+                .state('library.guide', {
+                    url: '/guide',
+                    templateUrl: './_modules/Library/_views/userguide/guide.html',
+                    controller: 'LibraryController as library'
+                })
+                .state('library.legaldocs', {
+                    url: '/legaldocs',
+                    templateUrl: './_modules/Library/_views/legaldocs/legaldocs.html',
+                    controller: 'LibraryController as library'
+                })
+                .state('library.loanproducts', {
+                    url: '/loanproducts',
+                    templateUrl: './_modules/Library/_views/products/loanproducts.html',
+                    controller: 'LibraryController as library'
+                })
+                .state('library.matrix', {
+                    url: '/matrix',
+                    templateUrl: './_modules/Library/_views/matrix/matrix.html',
+                    controller: 'LibraryController as library'
+                })
+                .state('library.pdfapps', {
+                    url: '/pdfapps',
+                    templateUrl: 'angular/library/pdf-apps/pdfapps.html',
+                    controller: 'PdfAppsController',
+                    resolve: {
+                        InitialData: function($http, API_URL){
+                            var url = API_URL + '/pdfapps';
+                            return $http.get(url)
+                                .then(function(rsp){
+                                    return rsp.data.data;
+                                });
+                        }
+                    }
+                })
+                .state('library.polsprocs', {
+                    url: '/polsprocs',
+                    templateUrl: './_modules/Library/_views/policies/polsprocs.html',
+                    controller: 'LibraryController as library'
+                })
+                .state('library.resources', {
+                    url: '/resources',
+                    templateUrl: './_modules/Library/_views/resources/resources.html',
                     controller: 'LibraryController as library'
                 });
         });
