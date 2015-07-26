96550dfc2ec2a94e3e458cbae1737aa83c9e8bf6
Jon Gravois <jongravois@gmail.com>
2015-07-26 14:57:30 -0500

    Fixed Admin Accordion

--- _modules/Admin/admin.ctrl.js
+++ _modules/Admin/admin.ctrl.js
@@ -10,38 +10,45 @@
         function AdminController($state, AppFactory) {
             /* jshint validthis: true */
             var vm = this;
-            vm.feeders_section = false;
-            vm.library_section = false;
-            vm.loans_section = false;
-            vm.system_section = false;
-            vm.users_section = false;
+            closeAll();
+            vm.system_section = true;
 
             vm.ngcLink = function (slug) {
                 //alert(slug);
+                if(slug === 'reports') {
+                    closeAll();
+                }
                 $state.go('admin.' + slug);
             };
 
             vm.toggleSection = function(slug) {
+                closeAll();
                 switch(slug){
                     case 'feeders':
-                        vm.feeders_section = !vm.feeders_section;
+                        vm.feeders_section = true;
                         break;
                     case 'library':
-                        vm.library_section = !vm.library_section;
+                        vm.library_section = true;
                         break;
                     case 'loans':
-                        vm.loans_section = !vm.loans_section;
+                        vm.loans_section = true;
                         break;
                     case 'system':
-                        vm.system_section = !vm.system_section;
+                        vm.system_section = true;
                         break;
                     case 'users':
-                        vm.users_section = !vm.users_section;
+                        vm.users_section = true;
                         break;
                 };
             }
             
             //////////
-            
+            function closeAll() {
+                vm.feeders_section = false;
+                vm.library_section = false;
+                vm.loans_section = false;
+                vm.system_section = false;
+                vm.users_section = false;
+            }
         } // end function
 })();
