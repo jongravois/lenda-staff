da25b6345c6f45e129bfd81b9ceb8bd2843a183f
petrovitch <kennthompson@gmail.com>
2015-07-30 07:21:43 -0500

    Add files

---
+++ _modules/Reports/customer.budget.ctrl.js
@@ -0,0 +1,355 @@
+(function () {
+    'use strict';
+    angular
+        .module('ARM')
+        .controller('CustomerBudgetController', CustomerBudgetController);
+
+    CustomerBudgetController.$inject = ['$scope', '$http', '$filter', 'AppFactory'];
+
+    function CustomerBudgetController($scope, $http, $filter, AppFactory) {
+        $scope.AppFactory = AppFactory;
+
+        var columnDefs = [
+            {
+                headerName: 'Indicators',
+                field: 'status_left',
+                cellClass: 'text-center',
+                suppressSorting: true,
+                suppressSizeToFit: false,
+                templateUrl: '_modules/Reports/_views/_partials/status.icons.left.html',
+                width: 165
+            },
+            {
+                headerName: 'Region',
+                field: 'region',
+                cellClass: 'text-center',
+                cellRenderer: function (params) {
+                    if (params.data.region.toUpperCase() === 'N') {
+                        return 'North';
+                    } else if (params.data.region.toUpperCase() === 'S') {
+                        return 'South';
+                    } else if (params.data.region.toUpperCase() === 'E') {
+                        return 'East';
+                    } else if (params.data.region.toUpperCase() === 'W') {
+                        return 'West';
+                    }
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 85
+            },
+            {
+                headerName: 'Loc',
+                field: 'location',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 70
+            },
+            {
+                headerGroup: 'Crop',
+                //headerGroupShow: 'closed',
+                headerName: 'Year',
+                field: 'crop_year',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 85,
+                filter: 'number'
+            },
+            {
+                headerGroup: 'Crop',
+                headerName: 'Season',
+                field: 'season',
+                cellClass: 'text-center',
+                cellRenderer: function (params) {
+                    if (params.data.season.toUpperCase() == 'F') {
+                        return 'Fall';
+                    } else {
+                        return 'Spring';
+                    }
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 95
+            },
+            {
+                headerName: 'Analyst',
+                field: 'analyst',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 95
+            },
+            {
+                headerName: 'Farmer',
+                field: 'farmer',
+                cellClass: 'text-left',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 120
+            },
+            {
+                headerName: 'Applicant',
+                field: 'applicant',
+                cellClass: 'text-left',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 120
+            },
+            {
+                headerGroup: 'Loan',
+                //headerGroupShow: 'closed',
+                headerName: 'Type',
+                field: 'loan_type',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 80
+            },
+            {
+                headerGroup: 'Loan',
+                //headerGroupShow: 'closed',
+                headerName: 'Dist',
+                field: 'dist',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 80
+            },
+            {
+                headerGroup: 'Loan',
+                field: 'loan_date',
+                headerName: 'Date',
+                cellClass: 'text-center',
+                cellRenderer: function (params) {
+                    return moment(params.data.loan_date).format('MM/DD/YYYY');
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 80
+            },
+            {
+                headerGroup: '',
+                headerName: 'Agency',
+                field: 'agency',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 80
+            },
+            {
+                headerName: 'Status',
+                field: 'status',
+                cellClass: 'text-center',
+                suppressSorting: true,
+                suppressSizeToFit: false,
+                templateUrl: '_modules/Reports/_views/_partials/status.icon.html',
+                width: 70
+            },
+            {
+                headerGroup: 'Budget',
+                headerName: 'Budget',
+                field: 'total_budget_amount',
+                cellClass: function (params) {
+                    return (params.data.total_budget_amount ? 'text-right' : 'text-center');
+                },
+                cellRenderer: function (params) {
+                    return $filter('flexCurrency')(params.data.total_budget_amount, 0);
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 100
+            },
+            {
+                headerGroup: 'Budget',
+                headerName: 'Spent',
+                field: 'total_budget_spent',
+                cellClass: function (params) {
+                    return (params.data.total_budget_spent ? 'text-right' : 'text-center');
+                },
+                cellRenderer: function (params) {
+                    return $filter('flexCurrency')(params.data.total_budget_spent, 0);
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 100
+            },
+            {
+                headerGroup: 'Budget',
+                headerName: 'Available',
+                field: 'total_budget_remaining',
+                cellClass: function (params) {
+                    return (params.data.total_budget_remaining ? 'text-right' : 'text-center');
+                },
+                cellRenderer: function (params) {
+                    params.data.total_budget_remaining = params.data.total_budget_amount - params.data.total_budget_spent;
+                    return $filter('flexCurrency')(params.data.total_budget_remaining, 0);
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 100
+            },
+            {
+                headerGroup: 'Budget',
+                headerName: 'Budget',
+                field: 'total_percent_budget',
+                cellClass: function (params) {
+                    var total_percent_budget = params.data.total_budget_amount / params.data.total_budget_amount * 100;
+                    return (total_percent_budget ? 'text-right' : 'text-center');
+                },
+                cellRenderer: function (params) {
+                    var total_percent_budget = params.data.total_budget_amount / params.data.total_budget_amount * 100;
+                    return $filter('flexPercent')(total_percent_budget, 1);
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 100
+            },
+            {
+                headerGroup: 'Budget',
+                headerName: 'Spent',
+                field: 'total_percent_spent',
+                cellClass: function (params) {
+                    return (params.data.total_budget_spent ? 'text-right' : 'text-center');
+                },
+                cellRenderer: function (params) {
+                    return $filter('flexPercent')(params.data.total_percent_spent, 1);
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 100
+            },
+            {
+                headerGroup: 'Budget',
+                headerName: 'Available',
+                field: 'total_percent_remaining',
+                cellClass: function (params) {
+                    return (1 - params.data.total_percent_spent ? 'text-right' : 'text-center');
+                },
+                cellRenderer: function (params) {
+                    params.data.total_percent_remaining = 100 - params.data.total_percent_spent;
+                    return $filter('flexPercent')(params.data.total_percent_remaining, 1);
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 100
+            }
+        ];
+
+        $scope.hideIcons = function () {
+            $scope.icons = !$scope.icons;
+            if ($scope.icons) {
+                $scope.gridOptions.pinnedColumnCount = $scope.pin - 1;
+            } else {
+                $scope.gridOptions.pinnedColumnCount = $scope.pin;
+            }
+            $scope.gridOptions.api.onNewCols();
+            $scope.gridOptions.api.hideColumns(['status_left', 'status', 'status_right'], $scope.icons);
+            $scope.gridOptions.api.hideColumns(['total_budget_amount'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_budget'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_budget_spent'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_spent'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_budget_remaining'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_remaining'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['status_left', 'status', 'status_right'], $scope.icons);
+            $scope.gridOptions.api.setSortModel($scope.sortKeys);
+        }
+
+        $scope.showToolPanel = function () {
+            $scope.tools = !$scope.tools;
+            $scope.gridOptions.api.showToolPanel($scope.tools);
+        }
+
+        $scope.toggleDetail = function () {
+            $scope.detail = !$scope.detail;
+            $scope.gridOptions.api.onNewCols();
+            $scope.gridOptions.api.hideColumns(['total_budget_amount'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_budget'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_budget_spent'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_spent'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_budget_remaining'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_remaining'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['status_left', 'status', 'status_right'], $scope.icons);
+            $scope.gridOptions.api.setSortModel($scope.sortKeys);
+        }
+
+        $scope.toggleDollarsPercent = function () {
+            $scope.dollarsPercent = !$scope.dollarsPercent;
+            $scope.gridOptions.api.onNewCols();
+            $scope.gridOptions.api.hideColumns(['total_budget_amount'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_budget'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_budget_spent'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_spent'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_budget_remaining'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_remaining'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['status_left', 'status', 'status_right'], $scope.icons);
+            $scope.gridOptions.api.setSortModel($scope.sortKeys);
+        }
+
+        $scope.toggleHorizontal = function () {
+            $scope.horizontal = !$scope.horizontal;
+            if ($scope.horizontal) {
+                $scope.pin = 0;
+            } else {
+                if ($scope.icons) {
+                    $scope.pin = $scope.pins - 1;
+                } else {
+                    $scope.pin = $scope.pins;
+                }
+            }
+            $scope.gridOptions.pinnedColumnCount = $scope.pin;
+            $scope.gridOptions.api.onNewCols();
+            $scope.gridOptions.api.hideColumns(['total_budget_amount'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_budget'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_budget_spent'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_spent'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_budget_remaining'], !$scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['total_percent_remaining'], $scope.dollarsPercent);
+            $scope.gridOptions.api.hideColumns(['status_left', 'status', 'status_right'], $scope.icons);
+            $scope.gridOptions.api.setSortModel($scope.sortKeys);
+        }
+
+
+        $scope.gridOptions = {
+            columnDefs: columnDefs,
+            rowSelection: 'single',
+            rowDeselection: true,
+            rowData: null,
+            pinnedColumnCount: $scope.pin,
+            groupHeaders: true,
+            angularCompileRows: true,
+            angularCompileFilters: true,
+            angularCompileHeaders: true,
+            enableColResize: true,
+            enableFilter: true,
+            enableSorting: true
+        };
+
+        $http.get("json/activity.detail.json")
+            .then(function (res) {
+                $scope.pins = 8;
+                $scope.pin = 0;
+                $scope.gridOptions.rowData = res.data;
+                $scope.gridHeight = Number(($scope.gridOptions.rowData.length + 2) * 30).toString();
+                $scope.gridOptions.api.onNewRows();
+                $scope.gridOptions.api.hideColumns(['total_percent_budget', 'total_percent_spent', 'total_percent_remaining'], true);
+                $scope.sortKeys = [
+                    {field: 'region', sort: 'asc'},
+                    {field: 'location', sort: 'asc'},
+                    {field: 'crop_year', sort: 'asc'},
+                    {field: 'season', sort: 'asc'},
+                    {field: 'analyst', sort: 'asc'},
+                    {field: 'farmer', sort: 'asc'},
+                    {field: 'applicant', sort: 'asc'},
+                    {field: 'dist', sort: 'asc'},
+                    {field: 'loan_type', sort: 'asc'},
+                ];
+                $scope.gridOptions.api.setSortModel($scope.sortKeys);
+                $scope.icons = false;
+                $scope.dollarsPercent = true;
+                $scope.detail = true;
+            });
+    }
+
+})();
---
+++ _modules/Reports/user.audit.ctrl.js
@@ -0,0 +1,200 @@
+(function () {
+    'use strict';
+    angular
+        .module('ARM')
+        .controller('UserAuditController', UserAuditController);
+
+    UserAuditController.$inject = ['$scope', '$http', '$filter', 'AppFactory'];
+
+    function UserAuditController($scope, $http, $filter, AppFactory) {
+        $scope.AppFactory = AppFactory;
+
+        var columnDefs = [
+            {
+                headerName: 'Region',
+                field: 'region',
+                cellClass: 'text-center',
+                cellRenderer: function(params) {
+                    if (params.data.region.toUpperCase() === 'N'){
+                        return 'North';
+                    } else if (params.data.region.toUpperCase() === 'S'){
+                        return 'South';
+                    } else if (params.data.region.toUpperCase() === 'E'){
+                        return 'East';
+                    } else if (params.data.region.toUpperCase() === 'W'){
+                        return 'West';
+                    }
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 85
+            },
+            {
+                headerName: 'Loc',
+                field: 'location',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 70
+            },
+            {
+                headerGroup: 'Crop',
+                headerName: 'Year',
+                //headerGroupShow: 'closed',
+                field: 'crop_year',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 90
+            },
+            {
+                headerGroup: 'Crop',
+                headerName: 'Season',
+                field: 'season',
+                cellClass: 'text-center',
+                cellRenderer: function(params) {
+                    if (params.data.season.toUpperCase() == 'F'){
+                        return 'Fall';
+                    } else {
+                        return 'Spring';
+                    }
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 90
+            },
+            {
+                headerName: 'Analyst',
+                field: 'analyst',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 90
+            },
+            {
+                headerName: 'Farmer',
+                field: 'farmer',
+                cellClass: 'text-left',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 120
+            },
+            {
+                headerName: 'Applicant',
+                field: 'applicant',
+                cellClass: 'text-left',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 120
+            },
+            {
+                headerGroup: 'Loan',
+                headerName: 'Type',
+                field: 'loan_type',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 90
+            },
+            {
+                headerGroup: "Audit",
+                headerName: 'Date',
+                field: 'audit_date',
+                cellClass: 'text-center',
+                cellRenderer: function(params) {
+                    return moment(params.data.audit_date).format('MM/DD/YYYY');
+                },
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 90
+            },
+            {
+                headerGroup: "Audit",
+                headerName: 'Time',
+                field: 'audit_time',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 90
+            },
+            {
+                headerGroup: "Audit",
+                headerName: 'User',
+                field: 'audit_user',
+                cellClass: 'text-center',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 90
+            },
+            {
+                headerGroup: "Audit",
+                headerName: 'Activity',
+                field: 'audit_activity',
+                cellClass: 'text-left',
+                suppressSorting: false,
+                suppressSizeToFit: false,
+                width: 300
+            }
+        ];
+
+        $scope.showToolPanel = function(){
+            $scope.tools = !$scope.tools;
+            $scope.gridOptions.api.showToolPanel($scope.tools);
+        }
+
+        $scope.toggleHorizontal = function(){
+            $scope.horizontal = !$scope.horizontal;
+            if ($scope.horizontal){
+                $scope.pin = 0;
+            } else {
+                if ($scope.icons) {
+                    $scope.pin = $scope.pins - 1;
+                } else {
+                    $scope.pin = $scope.pins;
+                }
+            }
+            $scope.gridOptions.pinnedColumnCount = $scope.pin;
+            $scope.gridOptions.api.onNewCols();
+            $scope.gridOptions.api.hideColumns(['status_left', 'status', 'status_right'], $scope.icons);
+            $scope.gridOptions.api.setSortModel($scope.sortKeys);
+        }
+
+        $scope.gridOptions = {
+            columnDefs: columnDefs,
+            rowSelection: 'single',
+            rowData: null,
+            pinnedColumnCount: $scope.pins,
+            groupHeaders: true,
+            angularCompileRows: true,
+            angularCompileFilters: true,
+            angularCompileHeaders: true,
+            enableColResize: true,
+            enableFilter: true,
+            enableSorting: true,
+            showToolPanel: false
+        };
+
+        $http.get("json/user.audit.json")
+            .then(function (res) {
+                $scope.pins = 7;
+                $scope.pin = 0;
+                $scope.gridOptions.rowData = res.data;
+                $scope.gridHeight = Number(($scope.gridOptions.rowData.length + 2) * 30).toString();
+                $scope.gridOptions.api.onNewRows();
+                $scope.sortKeys = [
+                    {field: 'region', sort: 'asc'},
+                    {field: 'location', sort: 'asc'},
+                    {field: 'crop_year', sort: 'asc'},
+                    {field: 'season', sort: 'asc'},
+                    {field: 'analyst', sort: 'asc'},
+                    {field: 'farmer', sort: 'asc'},
+                    {field: 'applicant', sort: 'asc'},
+                    {field: 'dist', sort: 'asc'},
+                    {field: 'loan_type', sort: 'asc'},
+                ];
+                $scope.gridOptions.api.setSortModel($scope.sortKeys);
+                $scope.tools = false;
+            });
+    }
+
+})();
