f6520dafc6e3eb70e1b4997d2f0229b50598f0a3
petrovitch <kennthompson@gmail.com>
2015-07-31 12:57:02 -0500

    Refactored report folders

--- _modules/Reports/reports.routes.js
+++ _modules/Reports/reports.routes.js
@@ -20,73 +20,73 @@
             })
             .state('reports.actdet', {
                 url: '/actdet',
-                templateUrl: '_modules/Reports/_views/activity.detail.tmpl.html',
+                templateUrl: '_modules/Reports/_views/activitydetail/activity.detail.tmpl.html',
                 controller: 'ActivityDetailController',
                 controllerAs: 'actdet'
             })
             .state('reports.lnman', {
                 url: '/lnman',
-                templateUrl: '_modules/Reports/_views/loan.management.tmpl.html',
+                templateUrl: '_modules/Reports/_views/loanmanagement/loan.management.tmpl.html',
                 controller: 'LoanManagementController',
                 controllerAs: 'lnman'
             })
             .state('reports.accrec', {
                 url: '/accrec',
-                templateUrl: '_modules/Reports/_views/account.reconciliation.tmpl.html',
+                templateUrl: '_modules/Reports/_views/accountreconciliation/account.reconciliation.tmpl.html',
                 controller: 'AccountReconciliationController',
                 controllerAs: 'accrec'
             })
             .state('reports.cashflow', {
                 url: '/cashflow',
-                templateUrl: '_modules/Reports/_views/cashflow.tmpl.html',
+                templateUrl: '_modules/Reports/_views/cashflow/cashflow.tmpl.html',
                 controller: 'CashflowController',
                 controllerAs: 'cashflow'
             })
             .state('reports.cusbud', {
                 url: '/cusbud',
-                templateUrl: '_modules/Reports/_views/customer.budget.tmpl.html',
+                templateUrl: '_modules/Reports/_views/customerbudget/customer.budget.tmpl.html',
                 controller: 'CustomerBudgetController',
                 controllerAs: 'cusbud'
             })
             .state('reports.actsum', {
                 url: '/actsum',
-                templateUrl: '_modules/Reports/_views/activity.summary.tmpl.html',
+                templateUrl: '_modules/Reports/_views/activitysummary/activity.summary.tmpl.html',
                 controller: 'ActivitySummaryController',
                 controllerAs: 'actsum'
             })
             .state('reports.fmrhis', {
                 url: '/fmrhis',
-                templateUrl: '_modules/Reports/_views/farmer.history.tmpl.html',
+                templateUrl: '_modules/Reports/_views/farmerhistory/farmer.history.tmpl.html',
                 controller: 'FarmerHistoryController',
                 controllerAs: 'fmrhis'
             })
             .state('reports.crops', {
                 url: '/crops',
-                templateUrl: '_modules/Reports/_views/crop.mix.tmpl.html',
+                templateUrl: '_modules/Reports/_views/cropmix/crop.mix.tmpl.html',
                 controller: 'CropMixController',
                 controllerAs: 'crops'
             })
             .state('reports.comapp', {
                 url: '/comapp',
-                templateUrl: '_modules/Reports/_views/committee.approval.tmpl.html',
+                templateUrl: '_modules/Reports/_views/committeeapproval/committee.approval.tmpl.html',
                 controller: 'CommitteeApprovalController',
                 controllerAs: 'comapp'
             })
             .state('reports.comcom', {
                 url: '/comcom',
-                templateUrl: '_modules/Reports/_views/committee.comment.tmpl.html',
+                templateUrl: '_modules/Reports/_views/committeecomment/committee.comment.tmpl.html',
                 controller: 'CommitteeCommentController',
                 controllerAs: 'comcom'
             })
             .state('reports.reqrpt', {
                 url: '/reqrpt',
-                templateUrl: '_modules/Reports/_views/required.tmpl.html',
+                templateUrl: '_modules/Reports/_views/required/required.tmpl.html',
                 controller: 'RequiredController',
                 controllerAs: 'reqrpt'
             })
             .state('reports.usradt', {
                 url: '/usradt',
-                templateUrl: '_modules/Reports/_views/user.audit.tmpl.html',
+                templateUrl: '_modules/Reports/_views/useraudit/user.audit.tmpl.html',
                 controller: 'UserAuditController',
                 controllerAs: 'usradt'
             });
