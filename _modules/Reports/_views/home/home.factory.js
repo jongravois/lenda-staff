dfd16baffc93ad67b3285e803c5497e4af4978ec
petrovitch <kennthompson@gmail.com>
2015-07-31 22:24:59 -0500

    Get more data for reports

--- _modules/Reports/_views/home/home.factory.js
+++ _modules/Reports/_views/home/home.factory.js
@@ -13,10 +13,16 @@
         };
         return publicAPI;
 
-        //////////
         function getJonsToy(loans) {
             var retro = _.map(loans, function(item){
                 var data = {};
+                data.has_addendum = item.has_addendum;
+                data.is_cross_collateralized = item.is_cross_collateralized;
+                data.bankruptcy_history = item.bankruptcy_history;
+                data.required_3party = item.required_3party;
+                data.added_land = item.added_land;
+                data.controlled_disbursement = item.controlled_disbursement;
+                data.attachments = item.attachments.length;
                 data.region = item.location.regions.region;
                 data.location = item.location.loc_abr;
                 data.crop_year = item.crop_year;
@@ -25,12 +31,16 @@
                 } else if (item.season == 'F'){
                     data.season = 'Fall';
                 }
-                data.analyst = item.analyst_abr;
+                data.analyst = item.analyst;
+                data.analyst_abr = item.analyst_abr;
                 data.farmer = item.farmer;
                 data.applicant = item.applicant;
-                data.applicant = item.loantype_abr;
-                data.applicant = item.dist;
-                data.applicant = item.orig_date;
+                data.loan_type = item.loan_type;
+                data.loantype_abr = item.loantype_abr;
+                data.distributor = item.distributor.distributor;
+                data.app_date = item.app_date;
+                data.status = item.status;
+                data.agencies = item.agencies;
                 data.interest = Number(item.fins.commit_arm) * Number(item.fins.int_percent_arm);
                 return data;
             });
