a14fdc0afac0bfff9b23768d0ab52ccc7d2f6a2a
petrovitch <kennthompson@gmail.com>
2015-08-03 06:03:36 -0500

    saving spot in work

--- _modules/Reports/reports.routes.js
+++ _modules/Reports/reports.routes.js
@@ -2,7 +2,7 @@
     'use strict';
     angular.module('ARM').config(function($stateProvider, $urlRouterProvider, API_URL) {
         $stateProvider
-            .state('reports', {
+            .state('arm.reports', {
                 url: '/reports',
                 abstract: true,
                 templateUrl: '_modules/Reports/_views/reports/reports.view.html',
@@ -13,67 +13,67 @@
                     }
                 }
             })
-            .state('reports.accrec', {
+            .state('arm.reports.accrec', {
                 url: '/accrec',
                 templateUrl: '_modules/Reports/_views/accountreconciliation/account.reconciliation.tmpl.html',
                 controller: 'AccountReconciliationController as accrec'
             })
-            .state('reports.actdet', {
+            .state('arm.reports.actdet', {
                 url: '/actdet',
                 templateUrl: '_modules/Reports/_views/activitydetail/activity.detail.tmpl.html',
                 controller: 'ActivityDetailController as actdet'
             })
-            .state('reports.actsum', {
+            .state('arm.reports.actsum', {
                 url: '/actsum',
                 templateUrl: '_modules/Reports/_views/activitysummary/activity.summary.tmpl.html',
                 controller: 'ActivitySummaryController as actsum'
             })
-            .state('reports.cashflow', {
+            .state('arm.reports.cashflow', {
                 url: '/cashflow',
                 templateUrl: '_modules/Reports/_views/cashflow/cashflow.tmpl.html',
                 controller: 'CashflowController as cashflow'
             })
-            .state('reports.comapp', {
+            .state('arm.reports.comapp', {
                 url: '/comapp',
                 templateUrl: '_modules/Reports/_views/committeeapproval/committee.approval.tmpl.html',
                 controller: 'CommitteeApprovalController as comapp'
             })
-            .state('reports.comcom', {
+            .state('arm.reports.comcom', {
                 url: '/comcom',
                 templateUrl: '_modules/Reports/_views/committeecomment/committee.comment.tmpl.html',
                 controller: 'CommitteeCommentController as comcom'
             })
-            .state('reports.crops', {
+            .state('arm.reports.crops', {
                 url: '/crops',
                 templateUrl: '_modules/Reports/_views/cropmix/crop.mix.tmpl.html',
                 controller: 'CropMixController as crops'
             })
-            .state('reports.cusbud', {
+            .state('arm.reports.cusbud', {
                 url: '/cusbud',
                 templateUrl: '_modules/Reports/_views/customerbudget/customer.budget.tmpl.html',
                 controller: 'CustomerBudgetController as cusbud'
             })
-            .state('reports.fmrhis', {
+            .state('arm.reports.fmrhis', {
                 url: '/fmrhis',
                 templateUrl: '_modules/Reports/_views/farmerhistory/farmer.history.tmpl.html',
                 controller: 'FarmerHistoryController as fmrhis'
             })
-            .state('reports.home', {
+            .state('arm.reports.home', {
                 url: '/home',
                 templateUrl: '_modules/Reports/_views/home/home.tmpl.html',
                 controller: 'HomeController as home'
             })
-            .state('reports.lnman', {
+            .state('arm.reports.lnman', {
                 url: '/lnman',
                 templateUrl: '_modules/Reports/_views/loanmanagement/loan.management.tmpl.html',
                 controller: 'LoanManagementController as lnman'
             })
-            .state('reports.reqrpt', {
+            .state('arm.reports.reqrpt', {
                 url: '/reqrpt',
                 templateUrl: '_modules/Reports/_views/required/required.tmpl.html',
                 controller: 'RequiredController as reqrpt'
             })
-            .state('reports.usradt', {
+            .state('arm.reports.usradt', {
                 url: '/usradt',
                 templateUrl: '_modules/Reports/_views/useraudit/user.audit.tmpl.html',
                 controller: 'UserAuditController as usradt'
