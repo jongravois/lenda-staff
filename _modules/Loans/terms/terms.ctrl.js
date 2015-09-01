15e5ea436f3c26e924cd4ea4b607ccf12de79c38
jgravois <jongravois@gmail.com>
2015-09-01 01:00:58 -0500

    Recovered Terms

--- _modules/Loans/terms/terms.ctrl.js
+++ _modules/Loans/terms/terms.ctrl.js
@@ -4,11 +4,12 @@
         .module('ARM')
         .controller('TermsController', TermsController);
 
-        TermsController.$inject = ['$rootScope', '$scope', 'AppFactory'];
+    TermsController.$inject = ['$rootScope', '$scope', '$state', 'AppFactory'];
 
-        function TermsController($rootScope, $scope, AppFactory){
-            $scope.saveTerms = function() {
-                alert('working');
-            };
-        } // end controller
+    function TermsController($rootScope, $scope, $state, AppFactory){
+        $scope.newapplications = $state.current.data.newapplications;
+        $scope.saveTerms = function() {
+            alert('working');
+        };
+    } // end controller
 })();
