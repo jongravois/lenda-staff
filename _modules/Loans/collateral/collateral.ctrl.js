adf046f7cf6981859943348d6874c0b9288d2ffe
jgravois <jongravois@gmail.com>
2015-09-01 07:29:43 -0500

    Recovered Collateral Tab

--- _modules/Loans/collateral/collateral.ctrl.js
+++ _modules/Loans/collateral/collateral.ctrl.js
@@ -4,9 +4,61 @@
         .module('ARM')
         .controller('CollateralsController', CollateralsController);
 
-        CollateralsController.$inject = ['$rootScope', '$scope', 'AppFactory'];
+    CollateralsController.$inject = ['$rootScope', '$scope', '$state', 'AppFactory'];
 
-        function CollateralsController($rootScope, $scope, AppFactory){
-            //console.log('LIENS', $scope.loan.priorlien);
-        } // end controller
+    function CollateralsController($rootScope, $scope, $state, AppFactory){
+        $scope.newapplications = $state.current.data.newapplications;
+        $scope.AppFactory = AppFactory;
+
+        $scope.showConditions = false;
+        $scope.showCropCollateral = false;
+        $scope.showEquipmentCollateral = false;
+        $scope.showRealEstateCollateral = false;
+        $scope.showOtherCollateral = false;
+        $scope.showCrossCollateral = false;
+
+        $scope.togShowConditions = function() {
+            $scope.showConditions = !$scope.showConditions;
+        };
+        $scope.togShowCropCollateral = function() {
+            $scope.showCropCollateral = !$scope.showCropCollateral;
+        };
+        $scope.togShowEquipmentCollateral = function() {
+            $scope.showEquipmentCollateral = !$scope.showEquipmentCollateral;
+        };
+        $scope.togShowRealEstateCollateral = function() {
+            $scope.showRealEstateCollateral = !$scope.showRealEstateCollateral;
+        };
+        $scope.togShowOtherCollateral = function() {
+            $scope.showOtherCollateral = !$scope.showOtherCollateral;
+        };
+        $scope.togShowCrossCollateral = function() {
+            $scope.showCrossCollateral = !$scope.showCrossCollateral;
+        };
+
+        $scope.calcCollateralAmount = function(obj) {
+            var val = Number(obj.mkt_value) * ((100 - Number(obj.discount))/100) - Number(obj.prior_lien);
+            if(val < 0) {
+                return 0;
+            } else {
+                return val;
+            }
+        }
+        $scope.saveOtherCollateral = function(type) {
+            alert('working');
+        };
+        $scope.deleteOtherCollateral = function(index, id) {
+            alert('working');
+        };
+        $scope.createOtherCollateral = function(type) {
+            alert('working');
+        };
+
+        $scope.calcPriorLienTotal = function() {
+            var liens = $scope.loan.prior_liens;
+            return _.sumCollection(liens, 'lien_amount');
+        }
+
+        console.log('COLLATERAL', $scope.loan.xcols);
+    } // end controller
 })();
